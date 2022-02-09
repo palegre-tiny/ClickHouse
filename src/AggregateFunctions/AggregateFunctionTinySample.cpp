916a342e52db58daf0f674e8e2a9e04882d05bd7
Pablo Alegre <palegre@tinybird.co>
2022-02-09 13:00:59 +0100

    Evolve to TinySum

--- src/AggregateFunctions/AggregateFunctionTinySample.cpp
+++ src/AggregateFunctions/AggregateFunctionTinySample.cpp
@@ -17,49 +17,57 @@ namespace ErrorCodes
     extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
 }
 
-struct TinyCountData
+struct TinySumData
 {
-    UInt64 counter = 0;
+    UInt64 total = 0;
 };
 
-class TinyCount : public IAggregateFunctionDataHelper<TinyCountData, TinyCount>
+class TinySum : public IAggregateFunctionDataHelper<TinySumData, TinySum>
 {
 public:
-    TinyCount(const DataTypes & argument_types_, const Array & params)
-        : IAggregateFunctionDataHelper<TinyCountData, TinyCount>(argument_types_, params)
+    TinySum(const DataTypes & argument_types_, const Array & params)
+        : IAggregateFunctionDataHelper<TinySumData, TinySum>(argument_types_, params)
     {
     }
 
-    String getName() const override { return "tinyCount"; }
-    
-    DataTypePtr getReturnType() const override { return std::make_shared<DataTypeUInt64>(); }
+    String getName() const override { return "tinySum"; }
+
+    DataTypePtr getReturnType() const override { return std::make_shared<DataTypeNumber<UInt64>>(); }
 
     bool allocatesMemoryInArena() const override { return false;}
 
-    void add(AggregateDataPtr __restrict place, const IColumn **, size_t, Arena *) const override
+    void add(AggregateDataPtr __restrict place, const IColumn **columns, size_t row_num, Arena *) const override
     {
-        this->data(place).counter ++;
+        UInt64 value = assert_cast<const ColumnVector<UInt64> &>(*columns[0]).getData()[row_num];
+        this->data(place).total += value;
     }
    
     void insertResultInto(AggregateDataPtr __restrict place, IColumn & to, Arena *) const override
     {
-        assert_cast<ColumnUInt64 &>(to).getData().push_back(this->data(place).counter);
+        assert_cast<ColumnUInt64 &>(to).getData().push_back(this->data(place).total);
     }
 
     NO_SERIALIZE
 };
 
 
-AggregateFunctionPtr createAggregateFunction_TinyCount(
-        const std::string &, const DataTypes & argument_types, const Array & params, const Settings *)
+AggregateFunctionPtr createAggregateFunction_TinySum(
+        const std::string &name, const DataTypes & argument_types, const Array & params, const Settings *)
 {
-    return AggregateFunctionPtr(new TinyCount(argument_types, params));
+    if (argument_types.size() != 1)
+        throw Exception("Aggregate function " + name + " requires one parameter.", ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH);
+
+    WhichDataType which(argument_types[0]);
+    if (which.idx == TypeIndex::UInt64)
+        return AggregateFunctionPtr(new TinySum(argument_types, params));
+
+    throw Exception("Invalid parameter type. Function " + name + " only supports UInt64", ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
 }
 
 void registerAggregateFunctionGroupTinySamples(AggregateFunctionFactory & factory)
 {
     AggregateFunctionProperties properties = {.returns_default_when_only_null = false, .is_order_dependent = true};
-    factory.registerFunction("tinyCount", {createAggregateFunction_TinyCount, properties});
+    factory.registerFunction("tinySum", {createAggregateFunction_TinySum, properties});
 }
 
 }
